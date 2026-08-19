650067c9a0097d6ae5662e39e1dd2b4ae5f627ad
alice-waweru <alicewaweru003@5676>
2026-08-19 19:04:54 +0300

    Configure PostgreSQL database and environment variables

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    """
+    Custom user model for Kids Love Toys.
+    
+    Customers can create accounts and place orders, while admins\owners can manage the products
+    
+    """
+    
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    
+    address = models.TextField(blank=True, null=True)
+    
+    is_customer = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.username
